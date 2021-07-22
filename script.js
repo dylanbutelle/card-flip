bd83186d16bfee741a7428d476388fb958515dd8
dbutelle <butelledylan@gmail.com>
2021-07-22 17:36:03 +0200

    Add commentary and changement Regex

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //Déclaration des variables
     let creditCardNumber = document.getElementById("number");
     let creditCardNumberCard = document.getElementById("cardNumber");
     let creditCardOwner = document.getElementById("owner");
@@ -7,17 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
     let creditCardExpirationCard = document.getElementById("dateCard");
     let creditCardCVC = document.getElementById("crypto");
     let creditCardCVCCard = document.getElementById("cryptoCard");
-    let card = document.getElementsByClassName("flip-card")[0]
     let cardInner = document.getElementsByClassName("flip-card-inner")[0]
-    let cptNbr = 0;
-    let maxNbr = creditCardNumber.getAttribute("maxLength");
-    let cptDate=1;
+
+    //Activation du flip de la carte si on est sur le champs CVC
     creditCardCVC.onfocus = function () {
         cardInner.style.transform = "rotateY(180deg)";
     }
     creditCardCVC.onblur = function () {
         cardInner.style.transform = "rotateY(0deg)";
     }
+    //Function de mise d'un espace
     function space(){
         if (creditCardNumber.value.length===4 ||creditCardNumber.value.length===9 || creditCardNumber.value.length===14) {
             creditCardNumber.value += " ";
@@ -25,26 +25,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
         }
     }
+    //Ecoute du numéro de carte
     creditCardNumber.addEventListener("keyup", function (e) {
-        console.log(creditCardNumber.value.length)
+        //S'il est vide
         if (creditCardNumber.value === "") {
+            //Affichage du texte de base
             creditCardNumberCard.innerHTML = "&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;";
 
         } else {
+            //On affichage le texte du input
             creditCardNumberCard.innerHTML = creditCardNumber.value;
         }
 
-
+            //On check si l'espace peut être mis
             space();
 
-
-
-
-
-
-
     })
+
     creditCardOwner.addEventListener("input", function () {
+
         if (creditCardOwner.value === "") {
             creditCardOwnerCard.innerHTML = "FULL NAME";
 
@@ -61,7 +60,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
             creditCardExpirationCard.innerHTML = creditCardExpiration.value;
         }
+        //Si on a entré le mois dans la date d'expiration
         if(creditCardExpiration.value.length===2){
+            //On affiche un slash
             creditCardExpiration.value+="/";
         }
     })
